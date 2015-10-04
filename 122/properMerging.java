190d70d3f38ea956f737630ff0246d6d532a988e
Shawn Xiang <xiangzuodong@hotmail.com>
2015-10-04 11:34:38 -0700

    Update properMerging.java

--- 122/properMerging.java
+++ 122/properMerging.java
@@ -1,43 +1,93 @@
+package homework;
+
+import java.util.Queue;
+import java.util.ArrayList;
+import java.util.LinkedList; 
 
 public class properMerging {
 
 	public static void main(String[] args) {
-		int [] p = {0, 5, 3, 4, 2, 1}; 
-		int [] q = {0, 3, 5 ,2, 4, 1}; 
-		// int [] p = {0, 1, 2, 3}; 
-		// int [] q = {0, 3, 2, 1}; 
-
-		int [] qHelper = new int[p.length];  
+		
+		
+		int [] p1 = {0, 1, 2, 3, 5, 4, 6};
+		int [] q1 = {0, 3, 2, 1, 6, 4, 5};
+		printInteger(p1, q1); 
+		System.out.println(properMerging(p1, q1));
+		
+		int [] p2 = {0, 1, 2, 3}; 
+		int [] q2 = {0, 3, 2, 1}; 
+		printInteger(p2, q2);
+		System.out.println(properMerging(p2, q2));
+		
+		int [] p3 = {0, 1, 4, 2, 3}; 
+		int [] q3 = {0, 2, 3, 1, 4}; 
+		printInteger(p3, q3); 
+		System.out.println(properMerging(p3, q3));
+		
+		int [] p4 = {0, 5, 3, 4, 2, 1}; 
+		int [] q4 = {0, 3, 5, 2, 4, 1}; 
+		printInteger(p4, q4); 
+		System.out.println(properMerging(p4, q4));
+	}
+		
+	public static Queue properMerging (int[] p, int q[]){
+		
+		
 		int [] pHelper = new int[q.length]; 
+		
+		Queue<Integer> pQueue = new LinkedList<Integer>(); 
+		Queue<Integer> qQueue = new LinkedList<Integer>(); 
+		
+		Queue<String> result = new LinkedList<String>(); 
 		
 		int i = 1; 
 		
 		for (i = 1; i < q.length; i++) {
-			qHelper[q[i]] = i; 
 			pHelper[p[i]] = i; 
+			pQueue.add(p[i]); 
+			qQueue.add(q[i]); 
 		}
 		
-		for (i = 1; i < p.length; i++){
-			
-			if (i >= qHelper[p[i]] && pHelper[p[i]] != 0 && qHelper[p[i]] != 0) {
-				
-				System.out.print(p[i] + " " + p[i] + "' "); 
-				
-				qHelper[p[i]] = 0; 
-				pHelper[q[i]] = 0; 
-			} else if (pHelper[p[i]] != 0) {
-				System.out.print(p[i] + " "); 
-				pHelper[p[i]] = 0;
-			}
-			
-			if ((pHelper[q[i]] > i || pHelper[q[i]] == 0 )&& qHelper[q[i]] != 0) {
-				System.out.print(q[i] + "' "); 
-				qHelper[q[i]] = 0;
+		while (!(pQueue.isEmpty() && qQueue.isEmpty())){
+		  try {
+			if (pHelper[(int)pQueue.peek()] != 0) {
+				for (i = pHelper[(int)pQueue.peek()]; i <= pHelper[(int)qQueue.peek()]; i++) {
+					int pPopOut = (int) pQueue.poll(); 
+					//System.out.print(pPopOut + " "); 
+					result.add(String.valueOf(pPopOut)); 
+					pHelper[pPopOut] = 0; 
+					 
+				}
+				//System.out.print(qQueue.poll() + "' "); 
 			} 
+			
+			result.add((String)(String.valueOf(qQueue.poll())+ "' ")); 
 		
+		  }
+		  catch (NullPointerException e) {
+			  while (!qQueue.isEmpty()){
+				  // System.out.print(qQueue.poll() + "' "); 
+				  result.add((String)(String.valueOf(qQueue.poll()) +  "' ")); 
+			  }
+		  }
 		}
 	
+		return result; 
 			
 	}
 
+	
+	public static void printInteger(int[] pArray, int [] qArray){
+		System.out.print(" p: "); 
+		for (int i: pArray){
+			if (i != 0)
+			System.out.print(i +  ", "); 
+		}
+		System.out.print(" q: "); 
+		for (int i: qArray){
+			if (i != 0)
+			System.out.print(i + ", "); 
+		}
+		System.out.print(" result: "); 
+	}
 }
